9ecb864c76ef87cbdb47c403e492195bc463b425
Siddharth Barjatya <46813781+Sid1512@users.noreply.github.com>
2022-05-27 01:00:45 +0530

    Landing Page

---
+++ cloud-app-react-main/src/App.js
@@ -0,0 +1,19 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import Build from "./components/Build";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <About />
+      <Build />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
