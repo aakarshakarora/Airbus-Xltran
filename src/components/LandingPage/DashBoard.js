181faf71dc10de600d1daf805812d80fb38c4ffc
Aakarshak Arora <aroraaakarshak@gmail.com>
2022-05-27 13:32:31 +0530

    landing page merged with signIn/Up

---
+++ src/components/LandingPage/DashBoard.js
@@ -0,0 +1,20 @@
+import NavBar from "./NavBar";
+import Hero from "./Hero";
+import About from "./About";
+import Build from "./Build";
+import Footer from "./Footer";
+
+
+function Dashboard() {
+    return (
+        <>
+            <NavBar/>
+            <Hero/>
+            <About/>
+            <Build/>
+            <Footer/>
+        </>
+    );
+}
+
+export default Dashboard;
