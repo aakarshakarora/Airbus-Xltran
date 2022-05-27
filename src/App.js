4c159d79addf0cbd5d0eef32cdeb4ce798185cf4
Siddharth Barjatya <46813781+Sid1512@users.noreply.github.com>
2022-05-27 19:42:01 +0530

    Work till example page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Reset from "./components/Reset/Reset";
 import Dashboard from "./components/LandingPage/DashBoard";
+import Example from "./components/FormPage/Example";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TempDashboard from "./tempDashboard";
 
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/reset" element={<Reset />} />
-          <Route exact path="/dashboard" element={<TempDashboard />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Router>
+				<Routes>
+					<Route exact path="/" element={<Login />} />
+					<Route exact path="/register" element={<Register />} />
+					<Route exact path="/reset" element={<Reset />} />
+					<Route exact path="/dashboard" element={<TempDashboard />} />
+					<Route exact path="/example" element={<Example />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
